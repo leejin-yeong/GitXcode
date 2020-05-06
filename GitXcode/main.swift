44959e5b302ac042206231117df3b683d4fb8409
Jinyoung Lee <cookie178@naver.com>
2020-05-06 13:21:35 +0900

    Initial Commit

---
+++ GitXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode
+//
+//  Created by Release on 2020/05/06.
+//  Copyright © 2020 JInyoung-Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
